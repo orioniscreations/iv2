40f4adb1c2771b0dff86a4328acd4e37ecbc17df
Orionis Creations <rysrps@gmail.com>
2021-02-02 14:39:17 -0400

    a test

--- js/highlights.js
+++ js/highlights.js
@@ -1,36 +1,39 @@
+HTML CSS JSResult Skip Results Iframe
 $(document).ready(function(){
 
-	$(".owl-carousel").owlCarousel({    
-		loop:true,
-		items:1,
-		margin:0,
-		stagePadding: 0,
-		autoplay:false  
-	});
-	
-	dotcount = 1;
-	
-	jQuery('.owl-dot').each(function() {
-		jQuery( this ).addClass( 'dotnumber' + dotcount);
-		jQuery( this ).attr('data-info', dotcount);
-		dotcount=dotcount+1;
-	});
-	
-	slidecount = 1;
-	
-	jQuery('.owl-item').not('.cloned').each(function() {
-		jQuery( this ).addClass( 'slidenumber' + slidecount);
-		slidecount=slidecount+1;
-	});
-	
-	jQuery('.owl-dot').each(function() {	
-		grab = jQuery(this).data('info');		
-		slidegrab = jQuery('.slidenumber'+ grab +' img').attr('src');
-		jQuery(this).css("background-image", "url("+slidegrab+")");  	
-	});
-	
-	amount = $('.owl-dot').length;
-	gotowidth = 100/amount;			
-	jQuery('.owl-dot').css("height", gotowidth+"%");
+  $(".owl-carousel").owlCarousel({    
+    loop:true,
+    items:1,
+    margin:0,
+    stagePadding: 0,
+    autoplay:false  
+  });
+  
+  dotcount = 1;
+  
+  jQuery('.owl-dot').each(function() {
+    jQuery( this ).addClass( 'dotnumber' + dotcount);
+    jQuery( this ).attr('data-info', dotcount);
+    dotcount=dotcount+1;
+  });
+  
+  slidecount = 1;
+  
+  jQuery('.owl-item').not('.cloned').each(function() {
+    jQuery( this ).addClass( 'slidenumber' + slidecount);
+    slidecount=slidecount+1;
+  });
+  
+  jQuery('.owl-dot').each(function() {  
+    grab = jQuery(this).data('info');   
+    slidegrab = jQuery('.slidenumber'+ grab +' img').attr('src');
+    jQuery(this).css("background-image", "url("+slidegrab+")");   
+  });
+  
+  amount = $('.owl-dot').length;
+  gotowidth = 100/amount;     
+  jQuery('.owl-dot').css("height", gotowidth+"%");
 
 });
+
+Resources1× 0.5× 0.25×Rerun
